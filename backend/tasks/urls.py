9fd2d43f91cc0725c005180a76322954eeed651a
Tejas <tejaspatil60404@gmail.com>
2025-11-25 21:51:22 +0530

    project setup django backend and frontend

---
+++ backend/tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # we'll add endpoints later:
+    # path('analyze/', views.analyze_tasks, name='analyze-tasks'),
+    # path('suggest/', views.suggest_tasks, name='suggest-tasks'),
+]
